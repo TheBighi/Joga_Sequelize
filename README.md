a30307ba6b57742a42c4b11705d0a8ab40034648
TheBighi <thebighix@gmail.com>
2026-02-25 15:35:05 +0200

    4. start

--- README.md
+++ README.md
@@ -40,6 +40,9 @@ npx sequelize-cli seed:generate --name demo-authors
 VAGA TAHTIS 
 
 ```
+npx sequelize-cli db:migrate
+npx sequelize-cli db:migrate:undo:all
+
 npx sequelize-cli db:seed:undo:all
 npx sequelize-cli db:seed:all
 ```
---
+++ seeders/20260225130800-demo-tags.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+  return queryInterface.bulkInsert("Tags", [
+      { name: "yogapractice", createdAt: new Date(), updatedAt: new Date() },
+      { name: "yogainspiration", createdAt: new Date(), updatedAt: new Date() },
+      { name: "wellness", createdAt: new Date(), updatedAt: new Date() },
+      { name: "nature", createdAt: new Date(), updatedAt: new Date() },
+      { name: "spiritual", createdAt: new Date(), updatedAt: new Date() },
+      { name: "sport", createdAt: new Date(), updatedAt: new Date() },
+      { name: "yogateacher", createdAt: new Date(), updatedAt: new Date() },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Tags", null, {});
+  }
+};
